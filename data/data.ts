cf7f40f1cebb40b2d6abc29bd13567c686f4b83d
Beau Dobbin <beau@beaunus.com>
2021-12-28 23:46:22 +0900

    feature: show byArtist and byMember mode

--- data/data.ts
+++ data/data.ts
@@ -1,16 +1,132 @@
+export interface Artist {
+  members: Person[];
+  id: string;
+  name: string;
+}
+
+export interface Person {
+  id: string;
+  name: string;
+  dateOfBirth: Date;
+}
+
 export interface Release {
   artistId: string;
   releaseDate: Date;
   title: string;
 }
 
+export const getArtists = (): Promise<Artist[]> =>
+  Promise.resolve([
+    {
+      id: "Billie Eilish",
+      members: [
+        {
+          dateOfBirth: new Date("December 18, 2001"),
+          id: "Billie Eilish",
+          name: "Billie Eilish",
+        },
+      ],
+      name: "Billie Eilish",
+    },
+    {
+      id: "The Beatles",
+      members: [
+        {
+          dateOfBirth: new Date("October 9, 1940"),
+          id: "John Lennon",
+          name: "John Lennon",
+        },
+        {
+          dateOfBirth: new Date("June 18, 1942"),
+          id: "Paul McCartney",
+          name: "Paul McCartney",
+        },
+        {
+          dateOfBirth: new Date("February 25, 1943"),
+          id: "George Harrison",
+          name: "George Harrison",
+        },
+        {
+          dateOfBirth: new Date("July 7, 1940"),
+          id: "Ringo Starr",
+          name: "Ringo Starr",
+        },
+      ],
+      name: "The Beatles",
+    },
+    {
+      id: "Itzy",
+      members: [
+        { dateOfBirth: new Date("May 26, 2000"), id: "Yeji", name: "Yeji" },
+        { dateOfBirth: new Date("July 21, 2000"), id: "Lia", name: "Lia" },
+        { dateOfBirth: new Date("December 9, 2003"), id: "Yuna", name: "Yuna" },
+        {
+          dateOfBirth: new Date("April 17, 2001"),
+          id: "Ryujin",
+          name: "Ryujin",
+        },
+        {
+          dateOfBirth: new Date("June 5, 2001"),
+          id: "Chaeryeong",
+          name: "Chaeryeong",
+        },
+      ],
+      name: "Sunmi",
+    },
+    {
+      id: "Sunmi",
+      members: [
+        { dateOfBirth: new Date("May 2, 1992"), id: "이선미", name: "이선미" },
+      ],
+      name: "Sunmi",
+    },
+  ]);
+
 export const getReleases = (): Promise<Release[]> =>
   Promise.resolve([
     {
-      artistId: "Sunmi",
-      releaseDate: new Date("22 October 1963"),
-      title: "Whatever",
+      artistId: "Billie Eilish",
+      releaseDate: new Date("March 29, 2019"),
+      title: "When We All Fall Asleep, Where Do We Go?",
     },
+    {
+      artistId: "Billie Eilish",
+      releaseDate: new Date("July 30, 2021"),
+      title: "Happier Than Ever",
+    },
+    {
+      artistId: "Itzy",
+      releaseDate: new Date("29 July 2019"),
+      title: "It's Icy",
+    },
+    { artistId: "Itzy", releaseDate: new Date("9 Mar 2020"), title: "It's Me" },
+    {
+      artistId: "Itzy",
+      releaseDate: new Date("17 Aug 2020"),
+      title: "Not Shy",
+    },
+    {
+      artistId: "Itzy",
+      releaseDate: new Date("30 Apr 2021"),
+      title: "Guess Who",
+    },
+    {
+      artistId: "Itzy",
+      releaseDate: new Date("24 Sep 2021"),
+      title: "Crazy In Love",
+    },
+    {
+      artistId: "Sunmi",
+      releaseDate: new Date("17 February 2014"),
+      title: "Full Moon",
+    },
+    {
+      artistId: "Sunmi",
+      releaseDate: new Date("4 September 2018"),
+      title: "Warning",
+    },
+    { artistId: "Sunmi", releaseDate: new Date("6 August 2021"), title: "1/6" },
     {
       artistId: "The Beatles",
       releaseDate: new Date("22 March 1963"),
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,86 @@
-import { getYear } from "date-fns";
+import { differenceInYears, getYear } from "date-fns";
 import _ from "lodash";
 import type { NextPage } from "next";
 import React, { useEffect } from "react";
 import ReactTooltip from "react-tooltip";
 
-import { getReleases, Release } from "../data/data";
+import { Artist, getArtists, getReleases, Release } from "../data/data";
 
 type Mode = "byArtist" | "byMember";
 
+const NUM_MS_IN_ONE_YEAR = 365 * 24 * 60 * 60 * 1000;
+
 const Home: NextPage = () => {
+  const [artists, setArtists] = React.useState<Artist[]>();
   const [releases, setReleases] = React.useState<Release[]>();
-  const [mode, setMode] = React.useState<Mode>("byArtist");
+  const [mode, setMode] = React.useState<Mode>("byMember");
 
   useEffect(() => {
-    getReleases().then(setReleases);
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    Promise.all([getReleases(), getArtists()]).then(([releases, artists]) => {
+      setReleases(releases);
+      setArtists(artists);
+    });
   }, []);
 
   const releasesByArtist = _.groupBy(releases, "artistId");
-  const yearsSorted = _.uniq(
-    releases?.map((release) => `${getYear(release.releaseDate)}`)
-  ).sort();
+  const artistIdsByPersonId = Object.fromEntries(
+    artists?.flatMap((artist) =>
+      artist.members.map((member) => [
+        member.id,
+        artists
+          .filter(({ members }) => members.some(({ id }) => id === member.id))
+          .map(({ id }) => id),
+      ])
+    ) ?? []
+  );
 
-  const TIMESTAMP_START = new Date(yearsSorted?.[0] || Date.now()).valueOf();
-  const TIMESTAMP_END = new Date(
-    yearsSorted?.[yearsSorted?.length - 1] || Date.now()
-  ).valueOf();
+  const personById = _.keyBy(
+    _.uniq(artists?.flatMap((artist) => artist.members)),
+    "id"
+  );
+
+  const releasesByPersonId = Object.fromEntries(
+    Object.entries(artistIdsByPersonId).map(([personId, artistIds]) => [
+      personId,
+      releases?.filter(({ artistId }) => artistIds.includes(artistId)),
+    ])
+  );
+
+  const years = _.sortBy(
+    _.uniq(releases?.map(({ releaseDate }) => getYear(releaseDate)))
+  );
+
+  const yearsToRender = _.range(
+    _.first(years) ?? 0,
+    (_.last(years) ?? 0) + 2
+  ).map(String);
+
+  const ages = _.sortBy(
+    _.uniq(
+      Object.entries(releasesByPersonId).flatMap(
+        ([personId, personReleases]) =>
+          personReleases?.map(({ releaseDate }) =>
+            differenceInYears(releaseDate, personById[personId].dateOfBirth)
+          ) ?? 0
+      )
+    )
+  );
+
+  const agesToRender = _.range(_.first(ages) ?? 0, (_.last(ages) ?? 0) + 2).map(
+    String
+  );
+
+  const TIMESTAMP_START =
+    mode === "byArtist"
+      ? new Date(yearsToRender?.[0] || Date.now()).valueOf()
+      : Number(agesToRender[0]) * NUM_MS_IN_ONE_YEAR;
+  const TIMESTAMP_END =
+    mode === "byArtist"
+      ? new Date(
+          yearsToRender?.[yearsToRender?.length - 1] || Date.now()
+        ).valueOf()
+      : Number(agesToRender[agesToRender.length - 1]) * NUM_MS_IN_ONE_YEAR;
 
   const modes: Mode[] = ["byArtist", "byMember"];
 
@@ -49,8 +105,8 @@ const Home: NextPage = () => {
         {mode === "byArtist" ? (
           <>
             <div />
-            <div className="flex justify-between">
-              {yearsSorted.map((year) => (
+            <div className="flex gap-1 justify-between">
+              {yearsToRender.map((year) => (
                 <div key={_.uniqueId("year")}>{year}</div>
               ))}
             </div>
@@ -96,7 +152,58 @@ const Home: NextPage = () => {
               ]
             )}
           </>
-        ) : null}
+        ) : (
+          <>
+            <div />
+            <div className="flex gap-1 justify-between">
+              {agesToRender.map((year) => (
+                <div key={_.uniqueId("year")}>{year}</div>
+              ))}
+            </div>
+            {Object.entries(releasesByPersonId).flatMap(
+              ([personId, personReleases]) => [
+                <div className="text-right" key={_.uniqueId("artistId")}>
+                  {personId}
+                </div>,
+                <div className="w-full" key={_.uniqueId("artistRelease")}>
+                  <div className="flex items-center">
+                    <div className="w-full h-0 border-b-2"></div>
+                  </div>
+                  <div className="relative">
+                    {personReleases?.map((release) => (
+                      <div key={_.uniqueId("release")}>
+                        <div
+                          className="absolute w-2 h-2 border-2"
+                          data-for={`${release.artistId}_${release.title}`}
+                          data-tip
+                          style={{
+                            left: `${
+                              (100 *
+                                (release.releaseDate.valueOf() -
+                                  personById[personId].dateOfBirth.valueOf() -
+                                  TIMESTAMP_START)) /
+                              (TIMESTAMP_END - TIMESTAMP_START)
+                            }%`,
+                          }}
+                        />
+                        <ReactTooltip
+                          effect="float"
+                          id={`${release.artistId}_${release.title}`}
+                          place="top"
+                          type="dark"
+                        >
+                          <div>{release.artistId}</div>
+                          <div>{release.releaseDate.toLocaleDateString()}</div>
+                          <div>{release.title}</div>
+                        </ReactTooltip>
+                      </div>
+                    ))}
+                  </div>
+                </div>,
+              ]
+            )}
+          </>
+        )}
       </div>
     </>
   );
