35d6d096c548db683596263ed85c36385d8ef41d
Beau Dobbin <beau@beaunus.com>
2022-01-01 12:45:44 +0900

    feature: ignore releases 30+ years after first release

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { differenceInYears } from "date-fns";
+import _ from "lodash";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { Release } from "../../data/data";
 import { getSpotifyClient } from "../../utils/spotify";
+
+const MAX_DURATION_OF_CAREER_IN_YEARS = 30;
 
 export default async function handler(
   _req: NextApiRequest,
@@ -18,11 +22,20 @@ export default async function handler(
     })
     .then((data) =>
       res.json(
-        data.body.items.map((item) => ({
-          artistId: "The Beatles",
-          releaseDate: new Date(item.release_date),
-          title: item.name,
-        }))
+        _.sortBy(
+          data.body.items.map((item) => ({
+            artistId: "The Beatles",
+            releaseDate: new Date(item.release_date),
+            title: item.name,
+          })),
+          "releaseDate"
+        ).filter(
+          (release, _index, [earliestRelease]) =>
+            differenceInYears(
+              release.releaseDate,
+              earliestRelease.releaseDate
+            ) < MAX_DURATION_OF_CAREER_IN_YEARS
+        )
       )
     )
     .catch(console.error);
